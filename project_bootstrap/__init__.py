e2c6428994107d6c15602db0cd33466d53acdbd6
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-01-29 17:09:44 +0800

    Update __init__.py

--- project_bootstrap/__init__.py
+++ project_bootstrap/__init__.py
@@ -1,4 +1,10 @@
 from .cli import main
+from .bootstrapper import Bootstrapper
+from .updater import SelfUpdater
+
+__all__ = ["Bootstrapper", "SelfUpdater"]
+
+__version__ = "0.1.0"
 
 if __name__ == "__main__":
     main()
